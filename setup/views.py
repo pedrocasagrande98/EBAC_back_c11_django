c1757828a63584a41de471084f29d02bc90bead5
pedrocasagrande98 <phccasagrande20@gmail.com>
2026-06-24 20:35:42 -0300

    'START ASYNC DJANGO'

---
+++ setup/views.py
@@ -0,0 +1,12 @@
+import asyncio
+import httpx
+from django.http import HttpResponse
+
+async def http_call_async(request):
+    for num in range(1, 6):
+        await asyncio.sleep(1)
+        print(num)
+    async with httpx.AsyncClient() as client:
+        r = await client.get("https://httpbin.org/")
+        print(r)
+    return HttpResponse("Requisição assíncrona com sucesso!")
